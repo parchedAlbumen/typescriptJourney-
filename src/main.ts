9c205daaf94299d33a8723df92c074d70cd53557
parchedAlbumen <kennethpamintuan2305@gmail.com>
2026-04-26 20:35:42 -0700

    learning typescript speedrun so i can do discord bot and then work on other projects

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,71 @@
-console.log("hello chat!");
-console.log("hi");
+class TaskManager {
+    tasks: Task[] = [];
 
-enum ContractStatus {
-    Permanent = 7, 
-    Temp,
-    Apprentice
+    addTask(task: Task) {
+        this.tasks.push(task);
+    }
+
+    updateStatus(id: number, newStatus: Status) {
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+            
+            if (task && task.id === id) {
+                task.status = newStatus;
+                console.log("successfully changed the status");
+                return;
+            }
+        }
+        
+        console.log("ID does not exist!");
+        return;
+    }
+
+    filterByStatus(status: Status) {
+        const newTasks: Task[] = [];
+        this.tasks.forEach(item => {
+            if (item.status === status) {
+                newTasks.push(item);
+            }
+        })
+        
+        return newTasks;
+    }
+    
+    filterByPriority(priority: Priority) {
+        const newTasks: Task[] = [];
+        this.tasks.forEach(item => {
+            if (item.priority === priority) {
+                newTasks .push(item);
+            }
+        })
+
+        return newTasks;
+    }
+
+    printTasks(tasks: Task[]) {
+        tasks.forEach(task => {
+            console.log(task);
+            console.log("\n");
+        })
+    }   
 }
 
-let employeeStatus: ContractStatus = ContractStatus.Temp;
-console.log(ContractStatus[employeeStatus]);    
+interface Task {
+    status: Status;
+    id: number;
+    title: string;
+    priority: Priority;
+    dueDate?: String; //question mark makes it optional
+}
+
+enum Status {
+    Todo,
+    InProgress,
+    Done
+}
+
+enum Priority {
+    Low,
+    Medium,
+    High
+}
